8c6655c1300c59c203a04d027275673a9e4fc5ce
iwill <minglq.9@gmail.com>
2021-01-22 19:00:16 +0800

    fea: Autolayout

---
+++ Sources/ExtSwift/Autolayout.swift
@@ -0,0 +1,65 @@
+//
+//  Autolayout.swift
+//  BJLiveBase
+//
+//  Created by MingLQ on 2020-12-29.
+//  Copyright © 2020 BaijiaYun. All rights reserved.
+//
+
+import Foundation
+
+#if os(iOS) // || os(tvOS)
+
+import UIKit
+
+extension UIResponder: ExtSwiftNameSpace {
+}
+
+public extension NameSpace where Base: UIView {
+    var safeAreaLayoutGuide: UILayoutGuide? {
+        guard #available(iOS 11, *) else {
+            return nil
+        }
+        return _base.safeAreaLayoutGuide
+    }
+}
+
+public extension NameSpace where Base: UIScrollView {
+    var contentLayoutGuide: UILayoutGuide? {
+        guard #available(iOS 11, *) else {
+            return nil
+        }
+        return _base.contentLayoutGuide
+    }
+    var frameLayoutGuide: UILayoutGuide? {
+        guard #available(iOS 11, *) else {
+            return nil
+        }
+        return _base.frameLayoutGuide
+    }
+}
+
+public extension NameSpace where Base: UIViewController {
+    var topLayoutGuide: UILayoutSupport? {
+        guard #available(iOS 11, *) else {
+            return _base.topLayoutGuide
+        }
+        return nil // Use `view.safeAreaLayoutGuide.topAnchor` instead
+    }
+    var bottomLayoutGuide: UILayoutSupport? {
+        guard #available(iOS 11, *) else {
+            return _base.bottomLayoutGuide
+        }
+        return nil // Use `view.safeAreaLayoutGuide.bottomAnchor` instead
+    }
+}
+
+#endif
+
+// #if os(macOS)
+// import AppKit
+// public typealias UIResponder = NSResponder
+// public typealias UIView = NSView
+// public typealias UILayoutGuide = NSLayoutGuide
+// public class UILayoutSupport {}
+// #endif
