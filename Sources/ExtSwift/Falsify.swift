c373520641a1ec309c08d54fead14eb95e1dde43
iwill <minglq.9@gmail.com>
2021-02-04 11:03:18 +0800

    opt: Falsify -> Falsifiable

--- Sources/ExtSwift/Falsify.swift
+++ Sources/ExtSwift/Falsify.swift
@@ -13,11 +13,11 @@ import CoreGraphics
 // MARK: - protocol
 
 /// - seealso: https://developer.mozilla.org/en-US/docs/Glossary/Falsy
-public protocol Falsify {
+public protocol Falsifiable {
     var isFalsy: Bool { get }
 }
 
-public extension Falsify {
+public extension Falsifiable {
     var isTruthy: Bool {
         return !isFalsy
     }
@@ -25,33 +25,33 @@ public extension Falsify {
 
 // MARK: - extension
 
-extension Bool   : Falsify { public var isFalsy: Bool { return !self       } }
-extension Int    : Falsify { public var isFalsy: Bool { return self == 0   } }
-extension Int8   : Falsify { public var isFalsy: Bool { return self == 0   } }
-extension Int16  : Falsify { public var isFalsy: Bool { return self == 0   } }
-extension Int32  : Falsify { public var isFalsy: Bool { return self == 0   } }
-extension Int64  : Falsify { public var isFalsy: Bool { return self == 0   } }
-extension UInt   : Falsify { public var isFalsy: Bool { return self == 0   } }
-extension UInt8  : Falsify { public var isFalsy: Bool { return self == 0   } }
-extension UInt16 : Falsify { public var isFalsy: Bool { return self == 0   } }
-extension UInt32 : Falsify { public var isFalsy: Bool { return self == 0   } }
-extension UInt64 : Falsify { public var isFalsy: Bool { return self == 0   } }
-extension Double : Falsify { public var isFalsy: Bool { return self == 0.0 } }
-extension Float  : Falsify { public var isFalsy: Bool { return self == 0.0 } }
+extension Bool   : Falsifiable { public var isFalsy: Bool { return !self       } }
+extension Int    : Falsifiable { public var isFalsy: Bool { return self == 0   } }
+extension Int8   : Falsifiable { public var isFalsy: Bool { return self == 0   } }
+extension Int16  : Falsifiable { public var isFalsy: Bool { return self == 0   } }
+extension Int32  : Falsifiable { public var isFalsy: Bool { return self == 0   } }
+extension Int64  : Falsifiable { public var isFalsy: Bool { return self == 0   } }
+extension UInt   : Falsifiable { public var isFalsy: Bool { return self == 0   } }
+extension UInt8  : Falsifiable { public var isFalsy: Bool { return self == 0   } }
+extension UInt16 : Falsifiable { public var isFalsy: Bool { return self == 0   } }
+extension UInt32 : Falsifiable { public var isFalsy: Bool { return self == 0   } }
+extension UInt64 : Falsifiable { public var isFalsy: Bool { return self == 0   } }
+extension Double : Falsifiable { public var isFalsy: Bool { return self == 0.0 } }
+extension Float  : Falsifiable { public var isFalsy: Bool { return self == 0.0 } }
 // public typealias Float32 = Float
 // public typealias Float64 = Double
 #if arch(i386) || arch(x86_64)
-extension Float80: Falsify { public var isFalsy: Bool { return self == 0.0 } }
+extension Float80: Falsifiable { public var isFalsy: Bool { return self == 0.0 } }
 #endif
 
-// NO: extension String : Falsify { public var isFalsy: Bool { return !isEmpty } }
-// NO: extension Array<Any>: Falsify { public var isFalsy: Bool { return !isEmpty } }
+// NO: extension String : Falsifiable { public var isFalsy: Bool { return !isEmpty } }
+// NO: extension Array<Any>: Falsifiable { public var isFalsy: Bool { return !isEmpty } }
 
-extension Optional: Falsify {
+extension Optional: Falsifiable {
     public var isFalsy: Bool {
         switch self {
         case .some(let v):
-            if let f = v as? Falsify {
+            if let f = v as? Falsifiable {
                 return f.isFalsy
             }
             else {
@@ -64,7 +64,7 @@ extension Optional: Falsify {
 }
 
 // OC Types
-extension NSNumber: Falsify { public var isFalsy: Bool { return self == 0.0 } }
+extension NSNumber: Falsifiable { public var isFalsy: Bool { return self == 0.0 } }
 #if canImport(CoreGraphics)
-extension CGFloat:  Falsify { public var isFalsy: Bool { return self == 0.0 } }
+extension CGFloat:  Falsifiable { public var isFalsy: Bool { return self == 0.0 } }
 #endif
