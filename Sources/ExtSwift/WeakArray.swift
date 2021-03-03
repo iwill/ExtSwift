7cb0d4b4dd97820b10b1f58558810756bb391a1f
Mr. Ming <minglq.9@gmail.com>
2021-03-03 10:41:39 +0800

    fea: WeakArray
    (cherry picked from commit a792e2cce7f29b1829bdc6b011aa623c66b6bd5b)

---
+++ Sources/ExtSwift/WeakArray.swift
@@ -0,0 +1,44 @@
+//
+//  WeakArray.swift
+//  ExtSwift
+//
+//  Created by Mr. Ming on 2021-03-02.
+//  Copyright (c) 2021 Mr. Ming <minglq.9@gmail.com>. Released under the MIT license.
+//
+
+import Foundation
+
+// Defined this protocol for `extension Array where Element: WeakBoxProtocol`
+// - seealso: https://forums.swift.org/t/extension-on-array-where-element-is-generic-type/10225/6
+public protocol WeakBoxProtocol {
+    associatedtype Wrapped: AnyObject
+    var wrapped: Wrapped? { get }
+}
+
+public class WeakBox<Wrapped: AnyObject>: WeakBoxProtocol {
+    public private(set) weak var wrapped: Wrapped?
+    public init(_ value: Wrapped) { wrapped = value }
+}
+
+public typealias WeakArray<Wrapped: AnyObject> = Array<WeakBox<Wrapped>>
+
+public extension Array where Element: WeakBoxProtocol/* , Element.Wrapped: AnyObject */ {
+    subscript(weak index: Index) -> Iterator.Element.Wrapped? {
+        get {
+            guard startIndex <= index && index < endIndex else { return nil }
+            return self[index].wrapped
+        }
+        set {
+            guard startIndex <= index && index <= endIndex else { return }
+            if let newValue = newValue {
+                self[index] = WeakBox(newValue) as! Element
+            }
+            else {
+                remove(at: index)
+            }
+        }
+    }
+    mutating func compact() {
+        removeAll { $0.wrapped == nil }
+    }
+}
