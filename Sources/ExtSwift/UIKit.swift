e63d57fd0c47cbd58836026432344b178bdcf12c
iwill <minglq.9@gmail.com>
2021-04-29 11:27:43 +0800

    fea: NSAttributedString

--- Sources/ExtSwift/UIKit.swift
+++ Sources/ExtSwift/UIKit.swift
@@ -14,11 +14,20 @@ public typealias UIResponder = NSResponder
 public typealias UIView = NSView
 #endif
 
+/// - seealso: https://developer.apple.com/documentation/swift/swift_standard_library/operator_declarations
+infix operator |: AdditionPrecedence
+
+// MARK: NSAttributedString
+
+public extension String {
+    static func | (string: String, attributes: [NSAttributedString.Key : Any]? = nil) -> NSAttributedString {
+        return NSAttributedString(string: string, attributes: attributes)
+    }
+}
+
 // MARK: UIColor
 
 #if os(iOS) || os(tvOS)
-/// - seealso: https://developer.apple.com/documentation/swift/swift_standard_library/operator_declarations
-infix operator |: AdditionPrecedence
 public extension UIColor {
     static func | (light: UIColor, dark: UIColor) -> UIColor {
         guard #available(iOS 13.0, tvOS 13.0, *) else { return light }
