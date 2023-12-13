002a428bf130d98a4a7d8d8654970cf1f6b49097
Míng <minglq.9@gmail.com>
2023-12-14 00:08:39 +0800

    meta: swift version and supported platforms version

--- Package.swift
+++ Package.swift
@@ -1,19 +1,20 @@
-// swift-tools-version:5.3
+// swift-tools-version:5.9
 
 import PackageDescription
 
 let package = Package(
     name: "ExtSwift",
     platforms: [
+        // supported min versions for `swift-tools-version:5.9`
         .iOS(.v12),
         .tvOS(.v12),
-        .macOS(.v10_14),
-        .watchOS(.v5)
+        .macOS(.v10_13),
+        .watchOS(.v4)
     ],
     products: [
-        .library(
-            name: "ExtSwift",
-            targets: ["ExtSwift"])
+        .library(name: "ExtSwift", targets: ["ExtSwift"]),
+        .library(name: "ExtSwift-Static", type: .static, targets: ["ExtSwift"]),
+        .library(name: "ExtSwift-Dynamic", type: .dynamic, targets: ["ExtSwift"])
     ],
     dependencies: [
         // .package(url: "https://github.com/iwill/ExCodable", from: "0.2.0")
