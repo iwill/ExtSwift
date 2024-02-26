f315a29e226056008b69b616ceeb6c52c064ed6c
Míng <minglq.9@gmail.com>
2024-02-27 02:37:37 +0800

    fea: String + ends(with: regex); Regex + suffixMatch + lastMatch

--- Sources/ExtSwift/Foundation/ExtSwift.swift
+++ Sources/ExtSwift/Foundation/ExtSwift.swift
@@ -13,6 +13,36 @@ internal struct ExtSwift {
 }
 
 // TODO: move to String+?.swift
+
+@available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
+extension BidirectionalCollection where Self.SubSequence == Substring {
+    public func ends(with regex: some RegexComponent) -> Bool {
+        guard let match = matches(of: regex).last else { return false }
+        return match.range.upperBound == endIndex
+    }
+}
+
+@available(macOS 13.0, iOS 16.0, watchOS 9.0, tvOS 16.0, *)
+extension Regex {
+    
+    public func suffixMatch(in string: String) throws -> Regex<Output>.Match? {
+        guard let match = string.matches(of: self).last else { return nil }
+        return match.range.upperBound == string.endIndex ? match : nil
+    }
+    
+    public func suffixMatch(in string: Substring) throws -> Regex<Output>.Match? {
+        return try suffixMatch(in: String(string))
+    }
+    
+    public func lastMatch(in string: String) throws -> Regex<Output>.Match? {
+        return string.matches(of: self).last
+    }
+    
+    public func lastMatch(in string: Substring) throws -> Regex<Output>.Match? {
+        return try lastMatch(in: String(string))
+    }
+}
+
 public extension String {
     init(localizedFormat: String, comment: String? = nil, _ arguments: CVarArg...) {
         self.init(format: NSLocalizedString(localizedFormat, comment: comment ?? ""), arguments)
